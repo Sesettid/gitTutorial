6b25df14a6a755ceaaae1c02f6c84796440cf2a9
Sesettid <57078212+Sesettid@users.noreply.github.com>
2020-09-14 22:01:50 -0400

    Add files via upload
    the  first program

---
+++ firstpro.java
@@ -0,0 +1,12 @@
+package practise;
+
+public class firstpro {
+	public static void main(String[] args) {
+		int a=5,b=6;
+		int c=a+b;
+		System.out.println(c);
+		System.out.println("hello world");
+		
+	}
+
+}
